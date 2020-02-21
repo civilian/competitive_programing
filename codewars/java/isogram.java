851e18f0144275b5ba7e75924c4380f73d323c9c
Oscar Chamat <chamatoscar@gmail.com>
2020-02-21 18:31:27 -0500

    Create isogram.java

---
+++ codewars/java/isogram.java
@@ -0,0 +1,14 @@
+public class isogram {
+    public static boolean  isIsogram(String str) {
+        int[] letters = new int['z'- 'a'];
+        str = str.toLowerCase();
+        for (int i=0; i<str.length(); i++){
+          int index = (str.charAt(i)) -'a';
+          if (letters[index] == 1){
+            return false;
+          }
+          letters[index]++;
+        }
+        return true;
+    } 
+}
