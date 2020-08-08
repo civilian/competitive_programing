88a7420b0b2d396d30d939a7b78ce6305a2dee93
Oscar Chamat <oscar@grydd.com>
2020-08-08 15:33:42 -0500

    hackerrank problem.

---
+++ hackerrank/hashTablesRansomNote.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the checkMagazine function below.
+def checkMagazine(magazine, note):
+    m = Counter(magazine)
+    n = Counter(note)
+    if len(n - m) == 0:
+        print("Yes")
+    else: 
+        print("No")
+
+if __name__ == '__main__':
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    checkMagazine(magazine, note)
