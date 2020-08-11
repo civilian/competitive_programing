a5b33af89c708237a70f9ca6da3190fc6606fc10
Oscar Chamat <oscar@grydd.com>
2020-08-11 13:51:11 -0500

    hackerrank sherlock-and-anagrams.py superfast.

--- hackerrank/sherlock-and-anagrams.py
+++ hackerrank/sherlock-and-anagrams.py
@@ -8,6 +8,7 @@ import sys
 import pprint
 from collections import Counter
 
+# https://stackoverflow.com/questions/13215789/comparing-anagrams-using-prime-numbers multiplication in primes gives unique response so it's a good way to identify anagrams.
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
 
 def sherlockAndAnagrams(s):
