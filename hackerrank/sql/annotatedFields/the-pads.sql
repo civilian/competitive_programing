9d21bd30b2809837b217e691263f4d7c9b443bb4
Oscar Chamat <oscar@grydd.com>
2021-01-11 19:54:48 -0500

    Adding hackerrank problems.

---
+++ hackerrank/sql/annotatedFields/the-pads.sql
@@ -0,0 +1,3 @@
+SELECT CONCAT(name, '(', LEFT(occupation, 1), ')') FROM occupations ORDER BY name;
+SELECT CONCAT('There are a total of ', COUNT(name), " ", LOWER(occupation), "s.") FROM occupations 
+    GROUP BY occupation ORDER BY COUNT(name), occupation;
---
+++ hackerrank/sql/userDefinedVariables/occupations.sql
@@ -0,0 +1,5 @@
+SET @r1=0, @r2=0, @r3=0, @r4=0;
+SELECT MIN(doctor), MIN(professor), MIN(singer), MIN(actor)
+FROM (
+    
+)
