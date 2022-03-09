c1e0b0486861d6c2cb47f514b490100745c05943
Oscar Chamat <chamatoscar@gmail.com>
2022-03-08 22:12:13 -0500

    Adding some problems just to remember the code.

---
+++ leetcode/9/937.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reorderLogFiles(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: List[str]
+        """
+        digits = []
+        letters = []
+        # divide logs into two parts, one is digit logs, the other is letter logs
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+
+        letters.sort(key=lambda x: x.split()[0])  # when suffix is tie, sort by identifier
+        letters.sort(key=lambda x: x.split()[1:])  # sort by suffix
+
+    result = letters + digits  # put digit logs after letter logs
+    return result
